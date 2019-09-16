f5ed819c2072110c766bc6262b75c5581971b3bc
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-09-16 15:17:00 +0200

    Added JPanel and JLabel, JButton on JPanel

--- src/swingDemo/demo01/SwingLessonOne.java
+++ src/swingDemo/demo01/SwingLessonOne.java
@@ -17,14 +17,32 @@ public class SwingLessonOne extends JFrame{
         int xPos = (dimension.width/2)-(this.getWidth()/2);
         int yPos = (dimension.height/2)-(this.getHeight()/2);
         this.setLocation(xPos,yPos);
-        this.setLocationRelativeTo(null);
+        //this.setLocationRelativeTo(null);
 
         this.setResizable(true);
         this.setVisible(true);
         //To close the frame by pressing x
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        //To set title
         this.setTitle("My First Frame");
+        //Set JPanel and JLabel
+        JPanel thePanel = new JPanel();
+
+        JLabel label1 = new JLabel("Tell me something");
+        thePanel.add(label1);
+        label1.setText("New Text");
+        label1.setToolTipText("Doesn't do anything.");
+
+        JButton button1 = new JButton("Send");
+        //button1.setBorderPainted(false);
+        button1.setContentAreaFilled(false);
+        button1.setText("New button");
+        button1.setToolTipText("This is a button.");
+        thePanel.add(button1);
+
+        this.add(thePanel);
+        this.show();
+
     }
 
 }
